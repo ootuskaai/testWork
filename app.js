f1996f9ed38a36ad74f1bd7d44f148d79780e217
Ting <ootuskaai@gmail.com>
2020-06-15 12:49:16 +0800

    aaa

--- app.js
+++ app.js
@@ -5,3 +5,5 @@ console.log('here is a hidden message');
 let selectElement = document.getElementById('date');
 selectElement.innerHTML = new Date().toDateString();
 
+console.log("add some text to check git (form testWork)");
+
